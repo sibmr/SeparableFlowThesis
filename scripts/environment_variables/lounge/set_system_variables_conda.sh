a00471d6dfb3f6ca6f747b3d9d5deb2130a9767a
sibmr <simon.bihlmaier@gmail.com>
2022-04-28 15:34:06 +0200

    added environment variables lounge

---
+++ scripts/environment_variables/lounge/set_system_variables_conda.sh
@@ -0,0 +1,7 @@
+export LD_LIBRARY_PATH="/home/bihlmasn/miniconda3/lib:$LD_LIBRARY_PATH"
+export LD_INCLUDE_PATH="/home/bihlmasn/miniconda3/include:$LD_INCLUDE_PATH"
+export CUDA_HOME="/usr/local/cuda-11.2"
+export PATH="/home/bihlmasn/miniconda3/bin:/usr/local/cuda-11.2/bin:$PATH"
+export CPATH="/usr/local/cuda-11.2/include"
+export CUDNN_INCLUDE_DIR="/usr/local/cuda-11.2/include"
+export CUDNN_LIB_DIR="/usr/local/cuda-11.2/lib64"
---
+++ scripts/environment_variables/lounge/set_system_variables_pip.sh
@@ -0,0 +1,11 @@
+# python
+export LD_LIBRARY_PATH="/home/bihlmasn/.local/share/virtualenvs/SeparableFlow-wh-ch6f8/lib/:$LD_LIBRARY_PATH"
+export LD_INCLUDE_PATH="/home/bihlmasn/.local/share/virtualenvs/SeparableFlow-wh-ch6f8/include/:$LD_INCLUDE_PATH"
+export PATH="/home/bihlmasn/.local/share/virtualenvs/SeparableFlow-wh-ch6f8/bin/:$PATH"
+
+# cuda
+export PATH="/usr/local/cuda-11.2/bin:$PATH"
+export CUDA_HOME="/usr/local/cuda-11.2"
+export CPATH="/usr/local/cuda-11.2/include"
+export CUDNN_INCLUDE_DIR="/usr/local/cuda-11.2/include"
+export CUDNN_LIB_DIR="/usr/local/cuda-11.2/lib64"
