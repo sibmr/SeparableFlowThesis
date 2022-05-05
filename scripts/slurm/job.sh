45a6e4d4accc32bdaf2cc1d6f6b468bfd3e66274
sibmr <simon.bihlmaier@gmail.com>
2022-05-05 17:40:08 +0200

    ajust slurm output and error paths

--- scripts/slurm/job.sh
+++ scripts/slurm/job.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
-#SBATCH --output=/home/user/project/checkpoints/train_sepflow/job_%j_output.txt
-#SBATCH --error=/home/user/project/checkpoints/train_sepflow/job_%j_error.txt
+#SBATCH --output=/home/bihlmasn/git/thesis/SeparableFlow/slurm_output/job_%j_output.txt
+#SBATCH --error=/home/bihlmasn/git/thesis/SeparableFlow/slurm_output/job_%j_error.txt
 
 /home/bihlmasn/git/thesis/SeparableFlow/scripts/slurm/run_me.sh
