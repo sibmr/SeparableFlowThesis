af9d8b722e97404f9bb04c0b5e581c3bf98d2a22
sibmr <simon.bihlmaier@gmail.com>
2022-06-27 15:17:56 +0200

    added training with 50k additional iterations on chairs

---
+++ checkpoints/train_originalLongerChairs/train.sh
@@ -0,0 +1,7 @@
+# exact same arguments as original implementation, except path related
+python train.py --experiment_name='train_originalLongerChairs' --stage='chairs'                                                                  --run_name='chairs' --gpu='0,1,2,3' --num_steps 100000 --batchSize 12 --testBatchSize=4 --lr=0.0004   --image_size 320 448  --wdecay 0.0001   --freeze_bn=0 --gamma=0.8   --start_epoch=0  --thread=16 
+python train.py --experiment_name='train_originalLongerChairs' --stage='things' --weights 'checkpoints/train_originalLongerChairs/chairs.pth'    --run_name='things' --gpu='0,1,2,3' --num_steps 100000 --batchSize 8  --testBatchSize=4 --lr 0.000125 --image_size 448 768  --wdecay 0.0001   --freeze_bn=1 --gamma=0.8     
+
+# Only C+T for now
+# python train.py --experiment_name='train_originalLongerChairs' --stage='sintel' --weights 'checkpoints/train_originalLongerChairs/things.pth' --run_name='sintel' --gpu='0,1,2,3' --num_steps 100000 --batchSize 8  --testBatchSize=4 --lr 0.000125 --image_size 384 832  --wdecay 0.00001  --freeze_bn=1 --gamma=0.85    
+# python train.py --experiment_name='train_originalLongerChairs' --stage='kitti'  --weights 'checkpoints/train_originalLongerChairs/sintel.pth' --run_name='kitti'  --gpu='0,1,2,3' --num_steps 50000  --batchSize 8  --testBatchSize=4 --lr 0.0001   --image_size 320 1024 --wdecay 0.00001  --freeze_bn=1 --gamma=0.85    
