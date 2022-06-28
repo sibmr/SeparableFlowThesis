71c24d2186fda0004f177aa82fee1d5dd04fa5da
sibmr <simon.bihlmaier@gmail.com>
2022-06-28 15:08:28 +0200

    fixed weights loading path

--- checkpoints/train_original_implementation/train.sh
+++ checkpoints/train_original_implementation/train.sh
@@ -1,6 +1,6 @@
 # exact same arguments as original implementation, except path related
-python train.py --experiment_name='train_original_implementation' --stage='chairs'                                                                  --run_name='chairs' --gpu='0,1,2,3' --num_steps 50000  --batchSize 12 --testBatchSize=4 --lr=0.0004   --image_size 320 448  --wdecay 0.0001   --freeze_bn=0 --gamma=0.8   --start_epoch=0  --thread=16 
-python train.py --experiment_name='train_original_implementation' --stage='things' --weights 'checkpoints/train_original_implementation/chairs.pth' --run_name='things' --gpu='0,1,2,3' --num_steps 100000 --batchSize 8  --testBatchSize=4 --lr 0.000125 --image_size 448 768  --wdecay 0.0001   --freeze_bn=1 --gamma=0.8     
+python train.py --experiment_name='train_original_implementation' --stage='chairs'                                                                          --run_name='chairs' --gpu='0,1,2,3' --num_steps 50000  --batchSize 12 --testBatchSize=4 --lr=0.0004   --image_size 320 448  --wdecay 0.0001   --freeze_bn=0 --gamma=0.8   --start_epoch=0  --thread=16 
+python train.py --experiment_name='train_original_implementation' --stage='things' --weights 'checkpoints/train_original_implementation/models/chairs.pth'  --run_name='things' --gpu='0,1,2,3' --num_steps 100000 --batchSize 8  --testBatchSize=4 --lr 0.000125 --image_size 448 768  --wdecay 0.0001   --freeze_bn=1 --gamma=0.8     
 
 # Only C+T for now
 # python train.py --experiment_name='train_original_implementation' --stage='sintel' --weights 'checkpoints/train_original_implementation/things.pth' --run_name='sintel' --gpu='0,1,2,3' --num_steps 100000 --batchSize 8  --testBatchSize=4 --lr 0.000125 --image_size 384 832  --wdecay 0.00001  --freeze_bn=1 --gamma=0.85    
