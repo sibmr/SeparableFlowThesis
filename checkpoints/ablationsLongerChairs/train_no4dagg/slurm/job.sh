52994445d4d482090bb9d78ee142c0d54b59d191
sibmr <simon.bihlmaier@gmail.com>
2022-10-05 11:57:01 +0200

    added refinement training for no 4d corr aggregation and 100K chairs

--- checkpoints/ablationsLongerChairs/train_no4dagg/slurm/job.sh
+++ checkpoints/ablationsLongerChairs/train_no4dagg/slurm/job.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=slurmqueue
 #SBATCH --mem=80gb 
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-gpu=2
 #SBATCH --gres=gpu:4
 #SBATCH --output=checkpoints/ablationsLongerChairs/train_no4dagg/slurm_output/job_%j_output.txt
 #SBATCH --error=checkpoints/ablationsLongerChairs/train_no4dagg/slurm_output/job_%j_error.txt
