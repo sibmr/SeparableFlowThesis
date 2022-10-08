a29aebb8c37376a36e5dbab2f063cc4397f0548e
sibmr <simon.bihlmaier@gmail.com>
2022-10-08 12:01:50 +0200

    refinement training for original 100K chairs model

--- checkpoints/train_originalLongerChairs/slurm/job.sh
+++ checkpoints/train_originalLongerChairs/slurm/job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="train_sepflow"
 #SBATCH --partition=slurmqueue
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-gpu=2
 #SBATCH --gres=gpu:4
 #SBATCH --output=checkpoints/train_originalLongerChairs/slurm_output/job_%j_output.txt
 #SBATCH --error=checkpoints/train_originalLongerChairs/slurm_output/job_%j_error.txt
